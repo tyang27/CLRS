f2ec4246cbcb3e454ac0acef8601cd1c02e132b6
Tony Yang <tyang27@jhu.edu>
2019-01-08 12:26:46 -0500

    graph is kinda messy, I'll refine it as I write more algorithms

--- graph.cpp
+++ graph.cpp
@@ -7,60 +7,95 @@ using namespace std;
 
 template <class V, class E>
 class Graph {
-
   public:
     Graph() {}
 };
 
+/*---------------------------------------------------------------------------*/
 template <class V, class E>
 class AdjacencyMatrixGraph : public Graph<V,E> {
-  vector<V> _vertices;
-  vector<vector<E> > _edges;
+  protected:
+    vector<V> _vertices;
+    vector<vector<E> > _edges;
   public:
-    AdjacencyMatrixGraph<V,E>(vector<V> v) : _vertices(v), _edges(v.size()) {
-      for (unsigned int i = 0; i < v.size(); i++) _edges[i] = vector<E>(v.size());
-    }
+    virtual void insert_edge(int i, int j) = 0;
+    virtual void remove_edge(int i, int j) = 0;
     void print();
+};
+
+/*---------------------------------------------------------------------------*/
+template <class V, class E>
+class AdjacencyMatrixDirGraph : public AdjacencyMatrixGraph<V,E> {
+  public:
+    AdjacencyMatrixDirGraph<V,E>(vector<V> v) {
+      this->_vertices = vector<V>(v);
+      this->_edges = vector<vector<E> >(v.size());
+      for (unsigned int i = 0; i < v.size(); i++)
+        this->_edges[i] = vector<E>(v.size());
+    }
+    void insert_edge(int i, int j) { this->_edges[i][j] += 1; }
+    void remove_edge(int i, int j) { this->_edges[i][j] -= 1; }
+};
+
+template <class V, class E>
+class AdjacencyMatrixUndirGraph : public AdjacencyMatrixGraph<V,E> {
+  public:
+    AdjacencyMatrixUndirGraph<V,E>(vector<V> v) {
+      this->_vertices = vector<V>(v);
+      this->_edges = vector<vector<E> >(v.size());
+      for (unsigned int i = 0; i < v.size(); i++)
+        this->_edges[i] = vector<E>(v.size());
+    }
+    void insert_edge(int i, int j) {
+      this->_edges[i][j] += 1;
+      this->_edges[j][i] += 1;
+    }
+    void remove_edge(int i, int j) {
+      this->_edges[i][j] -= 1;
+      this->_edges[j][i] -= 1;
+    }
 };
 
 template <class V, class E>
 void AdjacencyMatrixGraph<V,E>::print() {
-  /* Header. */
   cout << "\t";
   for (unsigned int i = 0; i < _vertices.size(); i++)
     cout << _vertices[i] << "\t";
   cout << endl;
 
   for (unsigned int i = 0; i < _vertices.size(); i++) {
-    /* Sider. */
     cout << _vertices[i] << "\t";
-
-    /* Content. */
     for (unsigned int j = 0; j < _vertices.size(); j++) {
-      if (i <= j) cout << "\t";
-      else cout << _edges[i][j] << "\t";
+      cout << _edges[i][j] << "\t";
     }
     cout << endl;
   }
 }
 
+/*---------------------------------------------------------------------------*/
 template <class V, class E>
 class AdjacencyListGraph: Graph<V,E> {
-  struct Vertex {
-  };
-  struct Edge {
-  };
+  protected:
+  map<V, vector<E> > _edges;
   public:
-    AdjacencyListGraph<V,E>() {cout << "Adjacency List constructor.\n";}
-    Vertex insert_vertex(V v);
-    Edge insert_edge(Vertex v1, Vertex v2);
+  void insert_vertex(V v) { }
+  void insert_edge(V v1, V v2) {
+    _edges[v1].push_back(v2);
+  }
+  void print();
 };
 
-int main() {
-  vector<string> v(10);
-  for (int i = 0; i < 10; i++) {
-    v[i] = 'a' + i;
+template <class V, class E>
+void AdjacencyListGraph<V,E>::print() {
+  for (auto it : _edges) {
+    cout << it.first << "\t";
+    for (auto itv : it.second) {
+      cout << itv << "\t";
+    }
+    cout << endl;
   }
-  AdjacencyMatrixGraph<string, string> g_mat(v);
-  g_mat.print();
+}
+/*---------------------------------------------------------------------------*/
+
+int main() {
 }
